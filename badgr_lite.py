8b47b0b8183f6d7c6fc418f5722c22bfbfb9bf73
Glen Jarvis <glen@glenjarvis.com>
2019-09-12 11:05:51 -0700

    Add test_verifies_token_file_exists unit test

--- badgr_lite.py
+++ badgr_lite.py
@@ -1,8 +1,25 @@
 """BadgrLite module for automating Badr awards (assertions)"""
 
+import os
+
+
+class TokenFileNotFoundError(BaseException):
+    """Token file not found
+
+    The token_file argument that you passed into BadgrLite is not found.
+    Please consider:
+        - Using using `prime_initial_token` (see Installation instructions)
+        - Checking the filename is correct
+        - Ensuring the filename exists and is in JSON format
+    """
+
+
 class BadgrLite:
     # pylint: disable=R0903
     # pylint: disable=C0111
 
     def __init__(self, token_file):
-        pass
+        if not os.path.exists(token_file):
+            raise TokenFileNotFoundError(
+                "Token File Not Found.",
+                TokenFileNotFoundError.__doc__)
--- test_badgr_lite.py
+++ test_badgr_lite.py
@@ -1,7 +1,10 @@
 """badgr_lite unit tests"""
 
 import unittest
+
 from badgr_lite import BadgrLite
+import badgr_lite
+
 
 class TestBadgrLiteMethods(unittest.TestCase):
     # pylint: disable=R0201
@@ -9,11 +12,17 @@ class TestBadgrLiteMethods(unittest.TestCase):
 
     def test_instantiates_badgr_lite_class(self):
         """It instantiates a BadgrLite class"""
-        BadgrLite(token_file=None)
+        BadgrLite(token_file='./previously_created_token_file.json')
 
     def test_takes_a_token_file(self):
         """It takes a token file argument"""
-        BadgrLite(token_file='./test_token_file.json')
+        BadgrLite(token_file='./previously_created_token_file.json')
+
+    def test_verifies_token_file_exists(self):
+        """It verifies Token file exists"""
+        with self.assertRaises(badgr_lite.TokenFileNotFoundError):
+            BadgrLite(token_file='./non_existent_token_file.json')
+
 
 if __name__ == '__main__':
     unittest.main()
