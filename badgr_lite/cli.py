afbfd19a2a4ffd3dc835a4ac4c2940de308171ae
Glen Jarvis <glen@glenjarvis.com>
2019-09-20 15:32:59 -0700

    Add test_token_file_option test

--- badgr_lite/cli.py
+++ badgr_lite/cli.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
+# pylint: disable=E1120
+
 """Console script for badgr_lite."""
 
 import click
 
 
 @click.group()
-def main():
+@click.option('--token-file', type=click.Path(exists=True),
+              default='./token.json',
+              help="File holding token credentials")
+def main(token_file):
     """Automate Badgr tasks without the overhead of badgr-server"""
 
 
--- tests/cli.py
+++ tests/cli.py
@@ -13,12 +13,22 @@ from badgr_lite import cli
 class TestBadgrLiteCLI(unittest.TestCase):
     """Tests for `badgr_lite` package."""
 
+    def setUp(self):
+        self.runner = CliRunner()
+
     def test_main_cli_entry_point(self):
         """Installation calls into cli.main()"""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'Automate Badgr tasks without the overhead of badgr-server' in result.output
+        result = self.runner.invoke(cli.main)
+        self.assertEqual(0, result.exit_code)
+        self.assertIn('Automate Badgr tasks without the overhead of badgr-server', result.output)
+
+
+    def test_token_file_option(self):
+        """CLI takes --token argument"""
+
+        help_result = self.runner.invoke(cli.main, ['--help'])
+        assert help_result.exit_code == 0
+        assert '--token-file PATH  File holding token credentials' in help_result.output
 
 if __name__ == '__main__':
     unittest.main()
