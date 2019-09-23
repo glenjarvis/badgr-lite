668537062beaafad766f6b9dec69dec17ce1aa2e
Glen Jarvis <glen@glenjarvis.com>
2019-09-23 16:07:45 -0700

    Add test_cli_subcommand_list_badges test

--- badgr_lite/cli.py
+++ badgr_lite/cli.py
@@ -1,17 +1,40 @@
 # -*- coding: utf-8 -*-
-# pylint: disable=E1120
-
+# pylint: disable=E1120, R0913, R0903, C0103
 """Console script for badgr_lite."""
 
 import click
 
 
+from badgr_lite.models import BadgrLite
+
+
+class Config:
+    """Share configuration accross commands"""
+
+    def __init__(self):
+        self.token_file = None
+
+
+pass_config = click.make_pass_decorator(Config, ensure=True)
+
+
 @click.group()
-@click.option('--token-file', type=click.Path(exists=True),
+@click.option('--token-file', type=click.Path(),
               default='./token.json',
               help="File holding token credentials")
-def main(token_file):
+@pass_config
+def main(config, token_file):
     """Automate Badgr tasks without the overhead of badgr-server"""
+
+
+@main.command()
+@pass_config
+def list_badges(config):
+    """Pull and print a list of badges from server"""
+
+    badgr = BadgrLite(token_filename=config.token_file)
+    for badge in badgr.badges:
+        click.echo(badge)
 
 
 if __name__ == "__main__":
