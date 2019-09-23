b4383600ee447b72f183adad31e7eedc7a3822f7
Glen Jarvis <glen@glenjarvis.com>
2019-09-23 16:07:45 -0700

    Add test_cli_subcommand_list_badges test

--- tests/cli.py
+++ tests/cli.py
@@ -10,25 +10,36 @@ from click.testing import CliRunner
 
 from badgr_lite import cli
 
+
 class TestBadgrLiteCLI(unittest.TestCase):
     """Tests for `badgr_lite` package."""
 
     def setUp(self):
         self.runner = CliRunner()
 
-    def test_main_cli_entry_point(self):
+    def test_cli_main_entry_point(self):
         """Installation calls into cli.main()"""
         result = self.runner.invoke(cli.main)
         self.assertEqual(0, result.exit_code)
-        self.assertIn('Automate Badgr tasks without the overhead of badgr-server', result.output)
+        self.assertIn(
+            'Automate Badgr tasks without the overhead of badgr-server',
+            result.output)
 
-
-    def test_token_file_option(self):
+    def test_cli_token_file_option(self):
         """CLI takes --token argument"""
 
         help_result = self.runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--token-file PATH  File holding token credentials' in help_result.output
+        self.assertEqual(0, help_result.exit_code)
+        self.assertIn(
+            '--token-file PATH  File holding token credentials',
+            help_result.output)
+
+    def test_cli_subcommand_list_badges(self):
+        """CLI has subcommand list-badges"""
+
+        result = self.runner.invoke(cli.main, ['list-badges', '--help'])
+        self.assertEqual(0, result.exit_code)
+
 
 if __name__ == '__main__':
     unittest.main()
