4db7fc48f324b269fc33161a18c493b8916274b7
Glen Jarvis <glen@glenjarvis.com>
2019-11-22 15:03:24 -0800

    Add Python 3.8 Trove Classifier

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     description="Automate awarding Badgr (or equivalent) badges to recipients without the overhead of badgr-server",
     entry_points={
